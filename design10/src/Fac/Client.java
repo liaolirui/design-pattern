2ad0cc415ea6033a7c09296b451cdc08c08be654
liaolirui <50520066+liaolirui@users.noreply.github.com>
2020-05-03 01:17:08 +0800

    Add files via upload

---
+++ design10/src/Fac/Client.java
@@ -0,0 +1,13 @@
+package Fac;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GeneralSwitchFacade gsf=new GeneralSwitchFacade();
+		gsf.on();
+		System.out.println("-------------------------");
+		gsf.off();
+	}
+
+}
