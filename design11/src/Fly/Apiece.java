c7669ecabc5703206f7b5e9728e08197504fe98a
liaolirui <50520066+liaolirui@users.noreply.github.com>
2020-05-03 01:17:43 +0800

    Add files via upload

---
+++ design11/src/Fly/Apiece.java
@@ -0,0 +1,11 @@
+package Fly;
+
+public abstract class Apiece {
+	protected String inKind;
+	public Apiece(String inKind) {
+		this.inKind=inKind;
+	}
+	
+	//下棋时落子的位置
+	public abstract void Play(int x,int y);
+}
